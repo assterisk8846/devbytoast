4f98e99287e02578462bdafa92da112a863c4280
John <narcisopanotes1997@gmail.com>
2024-12-21 21:42:31 +0800

    first commit

---
+++ app/routes/home/home.jsx
@@ -0,0 +1,152 @@
+import gamestackTexture2Large from '~/assets/gamestack-list-large.jpg';
+import gamestackTexture2Placeholder from '~/assets/gamestack-list-placeholder.jpg';
+import gamestackTexture2 from '~/assets/gamestack-list.jpg';
+import gamestackTextureLarge from '~/assets/gamestack-login-large.jpg';
+import gamestackTexturePlaceholder from '~/assets/gamestack-login-placeholder.jpg';
+import gamestackTexture from '~/assets/gamestack-login.jpg';
+import sliceTextureLarge from '~/assets/slice-app-large.jpg';
+import sliceTexturePlaceholder from '~/assets/slice-app-placeholder.jpg';
+import sliceTexture from '~/assets/slice-app.jpg';
+import sprTextureLarge from '~/assets/spr-lesson-builder-dark-large.jpg';
+import sprTexturePlaceholder from '~/assets/spr-lesson-builder-dark-placeholder.jpg';
+import muking from '~/assets/mukin.jpg';
+import sampleServer from '~/assets/server-sample.jpg';
+import { Footer } from '~/components/footer';
+import { baseMeta } from '~/utils/meta';
+import { Intro } from './intro';
+import { Profile } from './profile';
+import { ProjectSummary } from './project-summary';
+import { useEffect, useRef, useState } from 'react';
+import config from '~/config.json';
+import styles from './home.module.css';
+
+
+// Prefetch draco decoader wasm
+export const links = () => {
+  return [
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_wasm_wrapper.js',
+      as: 'script',
+      type: 'text/javascript',
+      importance: 'low',
+    },
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_decoder.wasm',
+      as: 'fetch',
+      type: 'application/wasm',
+      importance: 'low',
+    },
+  ];
+};
+
+export const meta = () => {
+  return baseMeta({
+    title: 'MU Online Developer',
+    description: `Design portfolio of John Panotes — DevByToast Company that offers MU Online private server configuration and develperments.`,
+  });
+};
+
+export const Home = () => {
+  const [visibleSections, setVisibleSections] = useState([]);
+  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const intro = useRef();
+  const projectOne = useRef();
+  const projectTwo = useRef();
+  const projectThree = useRef();
+  const details = useRef();
+
+  useEffect(() => {
+    const sections = [intro, projectOne, projectTwo, details];
+
+    const sectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const section = entry.target;
+            observer.unobserve(section);
+            if (visibleSections.includes(section)) return;
+            setVisibleSections(prevSections => [...prevSections, section]);
+          }
+        });
+      },
+      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+    );
+
+    const indicatorObserver = new IntersectionObserver(
+      ([entry]) => {
+        setScrollIndicatorHidden(!entry.isIntersecting);
+      },
+      { rootMargin: '-100% 0px 0px 0px' }
+    );
+
+    sections.forEach(section => {
+      sectionObserver.observe(section.current);
+    });
+
+    indicatorObserver.observe(intro.current);
+
+    return () => {
+      sectionObserver.disconnect();
+      indicatorObserver.disconnect();
+    };
+  }, [visibleSections]);
+
+  return (
+    <div className={styles.home}>
+      <Intro
+        id="intro"
+        sectionRef={intro}
+        scrollIndicatorHidden={scrollIndicatorHidden}
+      />
+      <ProjectSummary
+        id="project-1"
+        sectionRef={projectOne}
+        visible={visibleSections.includes(projectOne.current)}
+        index={1}
+        title="Servers Designed to Represent Your Vision"
+        description="Customizing Your MU Online Private Server with Professionalism and Attention to Detail for an Extraordinary Gameplay Experience"
+        buttonText="View project"
+        buttonLink="/projects/smart-sparrow"
+        model={{
+          type: 'laptop',
+          alt: 'Smart Sparrow lesson builder',
+          textures: [
+            {
+              srcSet: `${sampleServer} 1280w, ${sampleServer} 2560w`,
+              placeholder: sprTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-2"
+        alternate
+        sectionRef={projectTwo}
+        visible={visibleSections.includes(projectTwo.current)}
+        index={2}
+        title="Fully Customizable Game Events Powered by Lua Scripting"
+        description="Build Dynamic Game Events That Attract Players and Unlock a New Standard of Gameplay"
+        buttonText="View website"
+        buttonLink="https://gamestack.hamishw.com"
+        model={{
+          type: 'laptop2',
+          alt: 'Smart Sparrow lesson builder',
+          textures: [
+            {
+              srcSet: `${muking} 1280w, ${muking} 2560w`,
+              placeholder: sprTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <Profile
+        sectionRef={details}
+        visible={visibleSections.includes(details.current)}
+        id="details"
+      />
+      <Footer />
+    </div>
+  );
+};
---
+++ app/routes/home/profile.jsx
@@ -0,0 +1,95 @@
+import profileImgLarge from '~/assets/nar.jpg';
+import profileImgPlaceholder from '~/assets/nar.jpg';
+import profileImg from '~/assets/profile.jpg';
+import { Button } from '~/components/button';
+import { DecoderText } from '~/components/decoder-text';
+import { Divider } from '~/components/divider';
+import { Heading } from '~/components/heading';
+import { Image } from '~/components/image';
+import { Link } from '~/components/link';
+import { Section } from '~/components/section';
+import { Text } from '~/components/text';
+import { Transition } from '~/components/transition';
+import { Fragment, useState } from 'react';
+import { media } from '~/utils/style';
+import katakana from './katakana.svg';
+import styles from './profile.module.css';
+
+const ProfileText = ({ visible, titleId }) => (
+  <Fragment>
+    <Heading className={styles.title} data-visible={visible} level={3} id={titleId}>
+      <DecoderText text="Lets talk" start={visible} delay={500} />
+    </Heading>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+      I'm Nar, also known as Toast in the MU development community. Are you planning to start your dream private server? I'd love to hear your ideas and discuss how we can bring your vision to life.
+    </Text>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+    Check out my past clients and feel free to ask them about me. I'm easy to work with and will guide you through every milestone of your server's development. I look forward to being your developer soon!
+    </Text>
+  </Fragment>
+);
+
+export const Profile = ({ id, visible, sectionRef }) => {
+  const [focused, setFocused] = useState(false);
+  const titleId = `${id}-title`;
+
+  return (
+    <Section
+      className={styles.profile}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      as="section"
+      id={id}
+      ref={sectionRef}
+      aria-labelledby={titleId}
+      tabIndex={-1}
+    >
+      <Transition in={visible || focused} timeout={0}>
+        {({ visible, nodeRef }) => (
+          <div className={styles.content} ref={nodeRef}>
+            <div className={styles.column}>
+              <ProfileText visible={visible} titleId={titleId} />
+              <Button
+                secondary
+                className={styles.button}
+                data-visible={visible}
+                href="/contact"
+                icon="send"
+              >
+                Send me a message
+              </Button>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.tag} aria-hidden>
+                <Divider
+                  notchWidth="64px"
+                  notchHeight="8px"
+                  collapsed={!visible}
+                  collapseDelay={1000}
+                />
+                <div className={styles.tagText} data-visible={visible}>
+                  About me
+                </div>
+              </div>
+              <div className={styles.image}>
+                <Image
+                  reveal
+                  delay={100}
+                  placeholder={profileImgPlaceholder}
+                  srcSet={`${profileImg} 480w, ${profileImgLarge} 960w`}
+                  width={960}
+                  height={1280}
+                  sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                  alt="Me smiling like a goofball at the Qwilr office in Sydney"
+                />
+                <svg className={styles.svg} data-visible={visible} viewBox="0 0 136 766">
+                  <use href={`${katakana}#katakana-profile`} />
+                </svg>
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </Section>
+  );
+};
